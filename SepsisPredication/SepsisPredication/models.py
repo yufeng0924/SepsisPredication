2a58c3f2940ee2b142e0a97719fc88831c311d6c
yufeng0924 <33246411+yufeng0924@users.noreply.github.com>
2024-07-18 21:35:31 +0800

    Add files via upload

---
+++ SepsisPredication/SepsisPredication/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class MyModel(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        app_label = "SepsisPredication"
